347ddcc801fdd1900125e2169718ac47af080d0b
Muhammad Ibrahim <[muhammad.ibrahim@retailistan.com]>
2017-12-21 11:52:24 +0500

    thissss is commit

---
+++ app/src/main/java/com/ibrahim/pickdrop/PickDropApplication.java
@@ -0,0 +1,17 @@
+package com.ibrahim.pickdrop;
+
+import android.app.Application;
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by faraz on 11/29/2017.
+ */
+
+public class PickDropApplication extends MultiDexApplication {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
